930e8929297cc8dcca3cba9411249e4d0c9213ea
soupaul_expedia <soupaul@expediagroup.com>
2025-04-26 13:53:43 +0530

    added basic web socket example

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import { rateLimit } from 'express-rate-limit';
 import helmet from 'helmet';
+import { body, validationResult } from 'express-validator';
 
 const whitelist = ['::1'];
 //192.168.1.10
@@ -10,8 +11,6 @@ const generalLimiter = rateLimit({
 	windowMs: 30 * 1000, // 15 minutes
 	limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   skip: (req, res) => {
-    console.log(req);
-    console.log("this is the request ip = "+req.ip);
     return whitelist.includes(req.ip);
   },
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
@@ -30,9 +29,28 @@ const strictLimiter = rateLimit({
 });
 
 const app = express();
+
+import { Server } from "socket.io";
+import http from 'http';
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  }
+});
 const port = 3000;
 
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    console.log(socket.id);
+    socket.emit('response', 'hi from the server');
+  });
+});
+
 app.use(cookieParser());
+app.use(express.json());
 app.use(express.static('./public'));
 app.use('/sample-get-request', strictLimiter); // Apply rate limiting to the specific route
 app.use(helmet.frameguard({ action: 'deny' })); // Prevent clickjacking
@@ -53,7 +71,6 @@ eventEmitter.on('greet', (name) => {
 eventEmitter.emit('greet', 'Alice');
 
 app.get('/', (req, res) => {
-    console.log(req.cookies);
     res.cookie('hello', 'new world');
     res.cookie('hello2', 'new world2');
     res.render('index');
@@ -68,10 +85,28 @@ app.get('/sample-get-request', (req, res) => {
     const k1 = req.query.k1;
     const k2 = req.query.k2;
     const k3 = req.query.k3;
-    console.log(`k1: ${k1}, k2: ${k2}, k3: ${k3}`);
     res.send(`hello world`);
 });
 
-app.listen(port, () => {
+const removeSpecialCharacters = (str) => {
+    return str.replace(/[^a-zA-Z0-9]/g, '');
+}
+
+const validateUserForSignUp = [
+    body('username').trim().customSanitizer((value) => removeSpecialCharacters(value)).isLength({ min: 6 }).withMessage('Username must be at least 6 chars long'),
+    body('email').isEmail().withMessage('Email is not valid'),
+    body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 chars long'),
+];
+
+app.post('/user/signup', validateUserForSignUp, (req, res) => {
+    const errors = validationResult(req).errors;
+    console.log(errors);
+    if (errors.length>0) {
+        return res.status(400).json({ errors });
+    }
+    res.send('User signed up');
+});
+
+server.listen(port, () => {
   console.log(`Express server listening at http://localhost:${port}`);
 });
